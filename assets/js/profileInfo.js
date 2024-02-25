bfd83a1a019e21548f2b27f574f70831e8a44703
Abhishek Jadav <abhijadav99@gmail.com>
2024-02-25 23:15:51 +0530

    Completed Edit functionality and modified UI
    Only Edit Image is left and also Password.

--- assets/js/profileInfo.js
+++ assets/js/profileInfo.js
@@ -12,11 +12,15 @@ profileInfoHandler.extWallet = document.getElementById("ETX_WALLET");
 profileInfoHandler.btnAppyDetails = document.getElementById("BTN_APPLYDETAILS");
 profileInfoHandler.btnWallet = document.getElementById("BTN_WALLET");
 profileInfoHandler.backToDashboard = document.getElementById("BTN_BACK_TO_DASHBOARD");
-profileInfoHandler.btnEditUsernameIcon = document.getElementById('editUsernameIcon');
-profileInfoHandler.btnEditPhoneIcon = document.getElementById('editPhoneIcon');
-profileInfoHandler.btnEditPasswordIcon = document.getElementById('editPasswordIcon');
+profileInfoHandler.btnEditUsernameIcon = document.getElementById('BTN_EDIT_USERNAME');
+profileInfoHandler.btnEditPhoneIcon = document.getElementById('BTN_EDIT_PHONE');
+profileInfoHandler.btnEditPasswordIcon = document.getElementById('BTN_EDIT_PASSWORD');
+profileInfoHandler.profilePhoto = document.getElementById("PROFILE_PHOTO");
+profileInfoHandler.validationUsername = document.getElementById("VALIDATION_USERNAME");
+profileInfoHandler.validationPhone = document.getElementById("VALIDATION_PHONE");
+profileInfoHandler.validationPassword = document.getElementById("VALIDATION_PASSWORD");
 
-
+//Back to Dashboard button
 profileInfoHandler.backToDashboard.addEventListener("click", (e) => {
   window.location.href = 'dashBoard.html';
   window.electron.back();
@@ -26,35 +30,69 @@ profileInfoHandler.backToDashboard.addEventListener("click", (e) => {
 profileInfoHandler.deviceId.innerHTML = `Your Device Id : ${requestHelper.getData("DEVICEID")}`;
 
 //Set UserName From Storage Initially
+window.electron.getCurrentUser((currentUser) => {
+  profileInfoHandler.profilePhoto.src = `${requestHelper.serverAddress}/mobileApis/userImgs/${currentUser.user_img}`;
+  profileInfoHandler.extUsername.value = currentUser.user_name;
+  profileInfoHandler.emailId.innerHTML = currentUser.user_email;
+  profileInfoHandler.extPhone.value = currentUser.user_phone;
+  profileInfoHandler.extPassword.value = currentUser.user_pass;
+  profileInfoHandler.extWallet.innerHTML = currentUser.user_wallet;
+});
 
-async function fetchUserProfile() {
-  window.electron.getCurrentUser((currentUser) => {
-    profileInfoHandler.extUsername.value = currentUser.user_name;
-    profileInfoHandler.emailId.innerHTML = currentUser.user_email;
-    profileInfoHandler.extPhone.value = currentUser.user_phone;
-    profileInfoHandler.extPassword.innerHTML = currentUser.user_pass;
-    profileInfoHandler.extWallet.innerHTML = currentUser.user_wallet;
-  });
+profileInfoHandler.validateInputs = () => {
+  // Reset previous validation messages
+  profileInfoHandler.validationUsername.style.display = "none";
+  profileInfoHandler.validationPhone.style.display = "none";
+  profileInfoHandler.validationPassword.style.display = "none";
+  profileInfoHandler.extUsername.classList.remove('invalid_edittext');
+  profileInfoHandler.extPhone.classList.remove('invalid_edittext');
+  profileInfoHandler.extPassword.classList.remove('invalid_edittext');
+
+  if (profileInfoHandler.extUsername.value.length < 3) {
+    profileInfoHandler.setInputError('Please enter Valid Username', profileInfoHandler.validationUsername, profileInfoHandler.extUsername)
+    return false;
+  }
+
+  if (profileInfoHandler.extPhone.value.length < 10) {
+    profileInfoHandler.setInputError('Please enter Valid Phone number', profileInfoHandler.validationPhone, profileInfoHandler.extPhone)
+    return false;
+  }
+
+  if (profileInfoHandler.extPassword.value.length < 8) {
+    profileInfoHandler.setInputError('Minimum 8 character required', profileInfoHandler.validationPassword, profileInfoHandler.extPassword)
+    return false;
+  }
+
+  return true;
+}
+
+profileInfoHandler.setInputError = (error, validationArea, etx) => {
+  validationArea.innerHTML = error;
+  validationArea.style.display = 'block'
+  etx.classList.add('invalid_edittext')
 }
 
 profileInfoHandler.btnAppyDetails.addEventListener("click", (e) => {
 
   e.preventDefault(); //Stop Form Submission
-
-  if (loginHandler.validateInputs()) {
-
-    requestHelper.requestServer({
-      requestPath: "userAccAuth.php", requestMethod: "POST", requestPostBody: {
-
-      }
-    }).then(response => response.json()).then(jsonResponse => {
-      if (jsonResponse.isTaskSuccess == 'true') {
-
-      }
-      else
-        throw new Error(jsonResponse.response_msg);
-    }).catch(error => loginHandler.setAuthenticationError(error));
-  }
+  window.electron.getCurrentUser((currentUser) => {
+    if (profileInfoHandler.validateInputs()) {
+      requestHelper.requestServer({
+        requestPath: "userUpdateProfile.php", requestMethod: "POST", requestPostBody: {
+          user_email: currentUser.user_email,
+          user_name: profileInfoHandler.extUsername.value,
+          user_phone: profileInfoHandler.extPhone.value,
+        }
+      }).then(response => response.json()).then(jsonResponse => {
+        if (jsonResponse.isTaskSuccess == 'true') {
+          //save into current app memory
+          window.electron.setCurrentUser(jsonResponse.userAccData);
+        }
+        else
+          throw new Error(jsonResponse.response_msg);
+      }).catch(error => console.log(error));
+    }
+  });
 });
 
 function toggleEditableState(input) {
@@ -76,4 +114,4 @@ profileInfoHandler.btnEditPhoneIcon.addEventListener('click', () => {
   toggleEditableState(profileInfoHandler.extPhone);
 });
 
-document.addEventListener('DOMContentLoaded', fetchUserProfile);
+
