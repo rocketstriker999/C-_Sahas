49244f685a8f5e378c0208fbc0d7d433a7e20843
rocketstriker999 <hammerbyte.nisarg@gmail.com>
2024-03-13 00:29:46 +0530

    -added back the login feature

--- assets/js/createAccount.js
+++ assets/js/createAccount.js
@@ -5,35 +5,32 @@ let createAccountHandler = {};
 
 createAccountHandler.deviceId = document.getElementById("DEVICE_ID");
 
-//createAccountHandler.etxUserName = document.getElementById("ETX_USERNAME");
-//createAccountHandler.etxEmail = document.getElementById("ETX_EMAIL");
-//createAccountHandler.etxPassWord = document.getElementById("ETX_PASSWORD");
+createAccountHandler.etxUserName = document.getElementById("ETX_USERNAME");
+createAccountHandler.etxEmail = document.getElementById("ETX_EMAIL");
+createAccountHandler.etxPassWord = document.getElementById("ETX_PASSWORD");
 createAccountHandler.etxPhone = document.getElementById("ETX_PHONE");
-//createAccountHandler.etxReferCode = document.getElementById("ETX_REFERCODE");
 
 createAccountHandler.btnGoogleLogin = document.getElementById("BTN_GOOGLE_LOGIN");
 
-//createAccountHandler.counterUserName = document.getElementById("COUNTER_USERNAME");
-//createAccountHandler.counterEmail = document.getElementById("COUNTER_EMAIL");
-//createAccountHandler.counterPassWord = document.getElementById("COUNTER_PASSWORD");
+createAccountHandler.counterUserName = document.getElementById("COUNTER_USERNAME");
+createAccountHandler.counterEmail = document.getElementById("COUNTER_EMAIL");
+createAccountHandler.counterPassWord = document.getElementById("COUNTER_PASSWORD");
 createAccountHandler.counterPhone = document.getElementById("COUNTER_PHONE");
-//createAccountHandler.counterReferCode = document.getElementById("COUNTER_REFERCODE");
 
-//createAccountHandler.validationUsername = document.getElementById('VALIDATION_USERNAME');
-//createAccountHandler.validationEmail = document.getElementById('VALIDATION_EMAIL');
-//createAccountHandler.validationPassword = document.getElementById('VALIDATION_PASSWORD');
+createAccountHandler.validationUsername = document.getElementById('VALIDATION_USERNAME');
+createAccountHandler.validationEmail = document.getElementById('VALIDATION_EMAIL');
+createAccountHandler.validationPassword = document.getElementById('VALIDATION_PASSWORD');
 createAccountHandler.validationPhone = document.getElementById('VALIDATION_PHONE');
-//createAccountHandler.validationRefercode = document.getElementById('VALIDATION_REFERCODE');
 
-//createAccountHandler.error = document.getElementById("ERROR");
+createAccountHandler.error = document.getElementById("ERROR");
 
-//createAccountHandler.btnLogin = document.getElementById("BTN_LOGIN");
-//createAccountHandler.btnCreateAccout = document.getElementById("BTN_SIGNUP");
+createAccountHandler.btnLogin = document.getElementById("BTN_LOGIN");
+createAccountHandler.btnCreateAccout = document.getElementById("BTN_SIGNUP");
 
 //Set Device Id From Storage Initially
 createAccountHandler.deviceId.innerHTML = `Your Device Id : ${requestHelper.getData("DEVICEID")}`;
 
-/*createAccountHandler.etxUserName.addEventListener("input", (e) => {
+createAccountHandler.etxUserName.addEventListener("input", (e) => {
     createAccountHandler.counterUserName.innerText = e.target.value.length + " / 22 characters";
 });
 
@@ -43,17 +40,14 @@ createAccountHandler.etxEmail.addEventListener("input", (e) => {
 
 createAccountHandler.etxPassWord.addEventListener("input", (e) => {
     createAccountHandler.counterPassWord.innerText = e.target.value.length + " / 16 characters";
-});*/
+});
 
 createAccountHandler.etxPhone.addEventListener("input", (e) => {
     createAccountHandler.counterPhone.innerText = e.target.value.length + " / 10 characters";
 });
 
-/*createAccountHandler.etxReferCode.addEventListener("input", (e) => {
-    createAccountHandler.counterReferCode.innerText = e.target.value.length + " / 10 characters";
-});*/
 
-/*createAccountHandler.btnLogin.addEventListener("click", (e) => {
+createAccountHandler.btnLogin.addEventListener("click", (e) => {
     e.preventDefault(); //Stop Form Submission
     window.location.href = 'login.html'
 });
@@ -61,7 +55,8 @@ createAccountHandler.etxPhone.addEventListener("input", (e) => {
 createAccountHandler.setSignUpError=(error)=>{
     createAccountHandler.error.style.display="block";
     createAccountHandler.error.innerHTML=error;
-}*/
+}
+
 createAccountHandler.setInputError=(error,validationArea,etx)=>{
     validationArea.textContent=error;
     validationArea.style.display='block'
@@ -71,21 +66,19 @@ createAccountHandler.setInputError=(error,validationArea,etx)=>{
 createAccountHandler.validateInputs = () => {
 
     // Reset previous validation messages
-    //createAccountHandler.validationUsername.style.display = "none";
-    //createAccountHandler.validationEmail.style.display = "none"
-    //createAccountHandler.validationPassword.style.display = "none"
+    createAccountHandler.validationUsername.style.display = "none";
+    createAccountHandler.validationEmail.style.display = "none"
+    createAccountHandler.validationPassword.style.display = "none"
     createAccountHandler.validationPhone.style.display = "none"
-    //createAccountHandler.validationRefercode.style.display = "none"
 
-    //createAccountHandler.etxUserName.classList.remove('invalid_edittext');
-    //createAccountHandler.etxEmail.classList.remove('invalid_edittext');
-    //createAccountHandler.etxPassWord.classList.remove('invalid_edittext');
+    createAccountHandler.etxUserName.classList.remove('invalid_edittext');
+    createAccountHandler.etxEmail.classList.remove('invalid_edittext');
+    createAccountHandler.etxPassWord.classList.remove('invalid_edittext');
     createAccountHandler.etxPhone.classList.remove('invalid_edittext');
-    //createAccountHandler.etxReferCode.classList.remove('invalid_edittext');
 
     // Perform validation
 
-    /*if (createAccountHandler.etxUserName.value == '') {
+    if (createAccountHandler.etxUserName.value == '') {
         createAccountHandler.setInputError('Please enter your Full name',createAccountHandler.validationUsername,createAccountHandler.etxUserName);
         return false;
     }
@@ -98,22 +91,19 @@ createAccountHandler.validateInputs = () => {
     if (createAccountHandler.etxPassWord.value.length < 8) {
         createAccountHandler.setInputError('Password must be at least 8 characters long',createAccountHandler.validationPassword,createAccountHandler.etxPassWord);
         return false;
-    }*/
+    }
 
     if (createAccountHandler.etxPhone.value.length < 10) {
         createAccountHandler.setInputError('Please enter valid phone number', createAccountHandler.validationPhone, createAccountHandler.etxPhone);
         return false;
     }
 
-    /*if (createAccountHandler.counterReferCode.value == '') {
-        createAccountHandler.setInputError('Please enter refer code',createAccountHandler.validationRefercode,createAccountHandler.etxReferCode);
-        return false;
-    }*/
+   
     return true;
 
 }
 
-/*
+
 createAccountHandler.btnCreateAccout.addEventListener("click", (e) => {
     e.preventDefault(); //Stop Form Submission
 
@@ -125,7 +115,6 @@ createAccountHandler.btnCreateAccout.addEventListener("click", (e) => {
                 user_email: createAccountHandler.etxEmail.value,
                 user_pass: createAccountHandler.etxPassWord.value,
                 user_phone: createAccountHandler.etxPhone.value,
-                refer_id:createAccountHandler.etxReferCode.value,
                 user_device: requestHelper.getData("DEVICEID")
             }
         }).then(response=> response.json()).then(jsonResponse => {
@@ -142,37 +131,3 @@ createAccountHandler.btnCreateAccout.addEventListener("click", (e) => {
     }
 });
 
-*/
-
-//google Login redirect to Google Signin Via Sahas Website
-createAccountHandler.btnGoogleLogin.addEventListener("click", (e) => {
-
-    if (createAccountHandler.validateInputs()) {
-
-        window.electron.googleLogin((currentUser) => {
-            requestHelper.requestServer({
-                requestPath: "userAccAuthGoogle.php", requestMethod: "POST", requestPostBody: {
-                    user_name: currentUser.user_name,
-                    user_email: currentUser.user_email,
-                    user_phone: createAccountHandler.etxPhone.value,
-                    user_pass: currentUser.user_pass,
-                    refer_id: currentUser.signup_refer_user_index,
-                    user_device: requestHelper.getData("DEVICEID")
-                }
-            }).then(response => response.json()).then(jsonResponse => {
-                if (jsonResponse.isTaskSuccess == 'true') {
-                    //save current user for next app run
-                    requestHelper.saveData("LOGGEDINUSEREMAIL", currentUser.user_email);
-                    requestHelper.saveData("LOGGEDINUSERPASSWORD", currentUser.user_pass);
-                    //save into current app memory
-                    window.electron.setCurrentUser(jsonResponse.userAccData);
-                    //redirect to dashboard
-                    window.location.href = 'dashBoard.html';
-                }
-                else
-                    throw new Error(jsonResponse.response_msg);
-            }).catch(error => createAccountHandler.setAuthenticationError(error));
-        });
-
-    }
-});
